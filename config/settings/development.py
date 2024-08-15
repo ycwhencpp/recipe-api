9b46da9a3ee4a5abd1de8a190801aefde2ff863b
Anurag choudhary <anurag.jay1002@gmail.com>
2024-08-15 12:56:47 +0530

    celery setup (#1)

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,10 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [
+    'localhost'
+    ,'127.0.0.1'
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
