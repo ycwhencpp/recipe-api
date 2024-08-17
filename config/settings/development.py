7bf3db18d920203778234ab646b21428d9bc178f
ycwhencpp <anurag.jay1002@gmail.com>
2024-08-17 09:51:21 +0530

    deploy warninings fixed

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     'localhost'
     ,'127.0.0.1'
@@ -9,3 +9,13 @@ ALLOWED_HOSTS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Security settings
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# If your entire site is served over HTTPS
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
